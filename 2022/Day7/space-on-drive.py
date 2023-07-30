1a4167d80e73989f61aff18cb8578bebcbe67477
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2023-07-30 23:06:33 +0530

    delete filetree

--- 2022/Day7/space-on-drive.py
+++ 2022/Day7/space-on-drive.py
@@ -1,7 +1,30 @@
+from filetree import Tree as dirtree
+
+
 def main():
-    ...
-    
-    
-    
+    with open("input.txt") as f:
+        commands = f.readlines()
+
+    filetree = get_filetree(commands)
+
+
+def get_filetree(commands):
+    tree = dirtree
+    tree.set_root("/")
+
+    for i in commands:
+        i = i.strip()
+        if "$" in i:  # i is a command
+            if "cd" in i:
+                _, loc = i.split(" ")
+
+            else:
+                ...
+            ...
+        else:
+            ...
+
+
 if __name__ == "__main__":
     main()
+ 
