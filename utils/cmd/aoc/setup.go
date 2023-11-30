3e321a5b11ea691fc6674cbc12cc8aa40c2a83cd
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2023-11-30 23:38:30 +0530

    add scaffolding for setting up directories and downloading inputs

--- utils/cmd/aoc/setup.go
+++ utils/cmd/aoc/setup.go
@@ -2,6 +2,7 @@ package aoc
 
 import (
 	"aoc/pkg/setup"
+	"fmt"
 
 	"github.com/spf13/cobra"
 )
@@ -10,10 +11,13 @@ var setupCmd = &cobra.Command{
 	Use:     "setup",
 	Short:   "Setup Advent of Code challenges",
 	Long:    "Takes input of the form `year/day` and creates directories and files for that challenge.\ne.g. `setup 2022/1` or `set `22/1`.\n\nSets up the entire year if only the year is provided\ne.g. `setup 2022` will download and setup workspace for all problems from 2022.",
-	Aliases: []string{"set","stp"},
+	Aliases: []string{"set", "stp"},
 	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		setup.SetupProject(args[0])
+		err := setup.SetupProject(args[0])
+		if err != nil {
+			fmt.Println(err)
+		}
 	},
 }
 
